9ab8486402cfdb699cf777c10d48a40b571015b5
Florin Lilea <florin.lilea94@gmail.com>
2020-11-09 13:06:39 +0100

    First File

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <h1>TE DREQ FIRST REACT APaP</h1>
+        <h1>TE DREQ FIRST REACT APaP</h1>
+        <Person/>
       </div>
+
     );
+
+    // return React.createElement(
+    //   'div',
+    //   {className:'App'},
+    //   React.createElement('h1', null, 'TeDREq')
+    // );
   }
 }
 
